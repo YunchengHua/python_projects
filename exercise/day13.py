88ce3a8d17ee495a0d781072a68dcdb1fedb191d
YunchengHua <hua_yuncheng@hotmail.com>
2019-01-17 15:10:02 +0800

    Add files via upload

---
+++ exercise/day13.py
@@ -0,0 +1,16 @@
+class DeepshareStudent:
+    school = 'deepshare'
+    print('=====>')
+    def __init__(self):
+        print('====init run===>')
+
+    def learn(self):
+        print('%s is learning' % self)
+
+    def eat(self):
+        print('is eating')
+
+    def sleep(self):
+        print('is sleeping')
+
+DeepshareStudent()
